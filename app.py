a5d53b49ed5a4b6981c6d17a7adb6c19d6eca6d1
Sid <siddharth.jha30@gmail.com>
2024-02-07 06:00:05 +0530

    added streamlit app in app.py

--- app.py
+++ app.py
@@ -34,3 +34,22 @@ def generate_gemini_content(transcript_text,prompt):
     model=genai.GenerativeModel("gemini-pro")
     response=model.generate_content(prompt+transcript_text)
     return response
+
+
+#streamlit app
+
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    print(video_id)
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)  #Thumbnail image
+
+if st.button("Get Detailed Notes"):
+    transcript_text=extract_transcript_api(youtube_link)
+
+    if transcript_text:
+        summary=generate_gemini_content(transcript_text,prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
